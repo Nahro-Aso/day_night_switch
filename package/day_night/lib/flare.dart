2db4177e1e99e1d61ffa4d1bec94f127b98c1dd5
Nahro Aso Othman <87245866+Nahro-Aso@users.noreply.github.com>
2022-03-19 13:44:55 +0300

    Add files via upload

---
+++ package/day_night/lib/flare.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Flare extends StatelessWidget {
+  final double size;
+  final Color color;
+
+  const Flare({Key? key, required this.size, required this.color})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: color,
+      ),
+    );
+  }
+}
